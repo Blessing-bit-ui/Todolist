5ada3840b482ab92f313d21c18aeb9562935969f
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-05-05 05:28:42 -0700

    added count state

--- mytodolist/src/Todolist.js
+++ mytodolist/src/Todolist.js
@@ -4,28 +4,32 @@ import { useState } from 'react'
 function Todolist(){
 const [input, setInput] = useState("");
 const [tasks, setTasks] = useState([]);
+const [count, setCount]=useState(0);
 
-const addTasks = () => {
-  if (input !== "") {
-    setTasks(...tasks, input);
+function addTask(){
+  if (input !== " ") {
+    setTasks([...tasks, input]);
+    setCount(count + 1)
     setInput("")
   }
 };
 
-    return(
-        <div>
-            <input
-             value={input}
-             onChange={(e)=>setInput(e.target.value)}
-             placeholder="Enter Tasks"
-            />
-            <button onClick={addTasks}>Add</button>
-            <ul>
-                {tasks.map((task,index)=>(
-                    <li key={index}>{task}</li>
-                ))}
-            </ul>
-        </div>
-    )
+    return (
+      <div >
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter Tasks"
+        />
+        <button onClick={addTask}>Add</button>
+        <h2>Things to do </h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+        <p> Number of things to do: {count}</p>
+      </div>
+    );
 }
 export default Todolist;
